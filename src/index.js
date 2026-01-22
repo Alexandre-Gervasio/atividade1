8c3d9937a973a041e04bf7ae2cf25be0d0445510
AleGerva <alexandrework010@gmail.com>
2026-01-22 17:05:36 -0300

    Ajustes finos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let nome = "Elyndor Thalanis";
-let xpTotal = 1200;
+let xpTotal = 800;
 let nivel
 
 if(xpTotal <= 1000){
@@ -20,40 +20,75 @@ else if(xpTotal > 7000 && xpTotal <= 8000){
 else if(xpTotal > 8000 && xpTotal <= 9000){
     nivel = "Ascendente"
 }
-else if(xpTotal > 9000 && xpTotal < 10000){
+else if(xpTotal > 9000 && xpTotal <= 10000){
     nivel = "Imortal"
 }
-else{xpTotal >= 10000
+else if(xpTotal > 10000){
     nivel = "Radiante"
 }
 
-switch (xpTotal) {
-    case 1:
-        nivel = "Ferro"
+console.log("Olá, " + nome + "!");
+console.log("Seu nível atual é: " + nivel + "." + " Você possui " + xpTotal + " pontos de XP.");
+
+for(let i = 1; i <= 1; i++){
+    xpTotal += 250;
+    console.log("------------------------------------------------------------------------------------------------------");
+    console.log("Parabéns, " + nome + "! Você ganhou 250 pontos de XP ao derrotar o boss desse andar do jogo " + i + "!");
+}
+switch (true) {
+    case xpTotal <= 1000:
+        nivel = "Ferro";
         break;
-    case 2:
-        nivel = "Bronze"
+    case xpTotal > 1000 && xpTotal <= 2000:
+        nivel = "Bronze";
         break;
-    case 3:
-        nivel = "Prata"
+    case xpTotal > 2000 && xpTotal <= 5000:
+        nivel = "Prata";
         break;
-    case 4:
-        nivel = "Ouro"
+    case xpTotal > 5000 && xpTotal <= 7000:
+        nivel = "Ouro";
         break;
-    case 5:
-        nivel = "Platina"
+    case xpTotal > 7000 && xpTotal <= 8000:
+        nivel = "Platina";
         break;
-    case 6:
-        nivel = "Ascendente"
+    case xpTotal > 8000 && xpTotal <= 9000:
+        nivel = "Ascendente";
         break;
-    case 7:
-        nivel = "Imortal"
+    case xpTotal > 9000 && xpTotal <= 10000:
+        nivel = "Imortal";
         break;
-    case 8:
-        nivel = "Radiante"
+    case xpTotal > 10000:
+        nivel = "Radiante";
         break;
     default:
-        console.log("Nível desconhecido");
+        nivel = "Desconhecido";
+        break;
 }
+// Regras de nível comentadas abaixo caso queira utilizar if/else ao invés de switch
+// if(xpTotal <= 1000){
+//     nivel = "Ferro"
+// }
+// else if(xpTotal > 1000 && xpTotal <= 2000){
+//     nivel = "Bronze"
+// }
+// else if(xpTotal > 2000 && xpTotal <= 5000){
+//     nivel = "Prata"
+// }
+// else if(xpTotal > 5000 && xpTotal <= 7000){
+//     nivel = "Ouro"
+// }
+// else if(xpTotal > 7000 && xpTotal <= 8000){
+//     nivel = "Platina"
+// }
+// else if(xpTotal > 8000 && xpTotal <= 9000){
+//     nivel = "Ascendente"
+// }
+// else if(xpTotal > 9000 && xpTotal <= 10000){
+//     nivel = "Imortal"
+// }
+// else if(xpTotal > 10000){
+//     nivel = "Radiante"
+// }
+// Fim das regras de nível
 
-console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+console.log("Você herói " + nome + " está no nível " + nivel + "!" +  " Com " + xpTotal + " pontos de XP.");
